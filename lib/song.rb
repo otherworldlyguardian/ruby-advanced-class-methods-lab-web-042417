bf0e0cdd609b0c0a4ca5c091cc1538c50b5176fc
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-28 11:24:37 -0400

    Love music

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,65 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|person| person.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(file)
+    song = Song.new
+    arr = file.split(" - ")
+    song.name = arr[1].split(".")[0]
+    song.artist_name = arr[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = Song.new
+    arr = file.split(" - ")
+    song.name = arr[1].split(".")[0]
+    song.artist_name = arr[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
   def self.all
     @@all
